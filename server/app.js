d9334860cbac32437bbebf16d5da568b07bb1589
rj901126-lgtm <rj901126@GMAIL.COM>
2026-08-05 06:48:21 +0530

    chore: remove express-mongo-sanitize completely

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import compression from 'compression';
-// import mongoSanitize from 'express-mongo-sanitize';
 import xss from 'xss-clean';
 import rateLimit from 'express-rate-limit';
 import RedisStore from 'rate-limit-redis';
@@ -74,7 +73,6 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // Data Sanitization against NoSQL query injection & XSS
-// app.use(mongoSanitize());
 app.use(xss());
 
 // Logging
